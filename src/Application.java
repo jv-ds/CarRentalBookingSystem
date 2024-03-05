a89dece8eea39a8aff9b4f8959b83b00b3f4683d
Jeevan <jvet0005@student.monash.edu>
2024-03-06 01:31:35 +1100

    Created Application class

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-        Student student = new Student("John", "12345");        //create object of type student
-        //student.setName("John");                          //call method .setName(String name) for type student
-        System.out.println(student.getName());
-        //System.out.println(student);                        //print name of class & memory location, but when you use toString(), it tells us what's inside the object
+        BookingSystem bookingSystem1 = new BookingSystem();
+        bookingSystem1.printStatus();
     }
 }
