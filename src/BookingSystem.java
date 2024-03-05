b2bdc72348a38203191e5e718c2e6e138b447b41
Jeevan <jvet0005@student.monash.edu>
2024-03-06 01:32:21 +1100

    Created BookingSystem class

---
+++ src/BookingSystem.java
@@ -0,0 +1,9 @@
+public class BookingSystem {
+    public void printStatus() {
+        System.out.println("Welcome to FIT2099 Booking System");
+        System.out.println("Thank you for visiting FIT2099 Booking System!");
+
+        Vehicle vehicle1 = new Vehicle("Honda", 2014, 14000.00);
+        System.out.println(vehicle1.toString());
+    }
+}
