d6741e055e666c3efe534ccdadbf7e4614d5a6ac
Jeevan <jvet0005@student.monash.edu>
2024-03-11 14:07:01 +1100

    Added array of vehicles, createVehicles() method and displayVehicles() method to BookingSystem

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,7 @@
 public class Application {
     public static void main(String[] args) {
-        BookingSystem bookingSystem1 = new BookingSystem();
+        BookingSystem bookingSystem1 = new BookingSystem(6);
+        bookingSystem1.createVehicles();
         bookingSystem1.printStatus();
     }
 }
--- src/BookingSystem.java
+++ src/BookingSystem.java
@@ -1,21 +1,30 @@
 public class BookingSystem {
     private Vehicle[] vehicles;
 
+    public BookingSystem(int size){
+        vehicles = new Vehicle[6];
+    }
 
+    public void createVehicles(){
+        vehicles[0] = new Sedan("Lexus", 2023, 200.00,500, "ELECTRIC");
+        vehicles[1] = new Sedan("BMW", 2022, 150, 600, "PETROL");
+        vehicles[2] = new SUV("Tesla", 2023, 300, 5, "ELECTRIC");
+        vehicles[3] = new SUV("Mercedes", 2020, 500, 7, "DIESEL");
+        vehicles[4] = new Truck("Ford", 2019, 400, 1.0);
+        vehicles[5] = new Truck("Volkswagen", 2020, 450, 1.2);
+    }
+
+    public void displayVehicles(){
+        for(int i = 0; i < vehicles.length; i++){
+            System.out.printf("\nBooking Item (%s)\n",i+1);
+            System.out.println(vehicles[i].toString());
+        }
+    }
 
     public void printStatus() {
-        System.out.println("Welcome to FIT2099 Booking System");
-        System.out.println("Thank you for visiting FIT2099 Booking System!");
-
-        Vehicle vehicle1 = new Sedan("Honda", 2014, 14000.00, 400, "PETROL");
-        System.out.println(vehicle1.toString());
-
-        Vehicle vehicle2 = new SUV("Honda", 2014, 14000.00, 5, "PETROL");
-        System.out.println(vehicle2.toString());
-
-        Vehicle vehicle3= new Truck("Ford", 2019, 400.00, 1.0);
-        System.out.println(vehicle3.toString());
+        displayVehicles();
     }
+
 
 
 }
